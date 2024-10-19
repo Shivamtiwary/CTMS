a921013d6869d6aa728d95bfee8375dd5b3fec6d
Shivamtiwary <tiwaryshivam777@gmail.com>
2024-10-19 22:16:14 +0530

    Your descriptive commit message

---
+++ index.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Covid Testing Management System</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/scrolling-nav.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">Covid (Covid-19)-TMS</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#about">About Covid (Covid-19) </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#services"> Covid (Covid-19) Symptoms</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#contact">Prevention</a>
+          </li>
+  <li class="nav-item active">
+            <a class="nav-link js-scroll-trigger" href="new-user-testing.php">Testing</a>
+          </li>
+           <li class="nav-item active">
+            <a class="nav-link js-scroll-trigger" href="live-test-updates.php">Live Updates</a>
+          </li>
+
+           <li class="nav-item active">
+            <a class="nav-link js-scroll-trigger" href="login.php">Admin</a>
+          </li>
+
+
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <header class="bg-primary text-white">
+    <div class="container text-center">
+      <h1>Covid (Covid-19)-TMS</h1>
+      <p class="lead">Covid (Covid-19) - Testing Management System</p>
+    </div>
+  </header>
+
+  <section id="about">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
+          <h2>About this page</h2>
+          <p class="lead">Coronavirus disease (COVID-19) is an infectious disease caused by the SARS-CoV-2 virus.
+Most people infected with the virus will experience mild to moderate respiratory illness and recover without requiring special treatment. However, some will become seriously ill and require medical attention. Older people and those with underlying medical conditions like cardiovascular disease, diabetes, chronic respiratory disease, or cancer are more likely to develop serious illness. Anyone can get sick with COVID-19 and become seriously ill or die at any age. </p>
+          <p class="lead">The virus can spread from an infected person’s mouth or nose in small liquid particles when they cough, sneeze, speak, sing or breathe. These particles range from larger respiratory droplets to smaller aerosols. It is important to practice respiratory etiquette, for example by coughing into a flexed elbow, and to stay home and self-isolate until you recover if you feel unwell.</p>
+         
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="services" class="bg-light">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
+          <h2>Symptoms may initially include one or several of the following:</h2>
+          <hr />
+<p><strong>Fever</strong><br />
+
+             <hr />
+<p><strong>Headache</strong><br />
+          <hr />
+<p><strong>Cough</strong><br />
+          <hr />
+<p><strong>Difficulty breathing</strong><br />
+
+
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="contact">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
+          <h2>Prevention</h2>
+  <ul>
+            <li>Practice handwashing regularly with soap and water</li>
+            <li>Avoid contact with sick bats or pigs</li>
+            <li>Avoid areas where bats are known to roost</li>
+            <li>Avoid eating or drinking products that could be contaminated by bats, such as raw date palm sap, raw fruit, or fruit that is found on the ground</li>
+            <li>Avoid contact with the blood or body fluids of any person known to be infected with NiV</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Footer -->
+  <footer class="py-5 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">Covid Testing Management System</p>
+    </div>
+    <!-- /.container -->
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom JavaScript for this theme -->
+  <script src="js/scrolling-nav.js"></script>
+
+</body>
+
+</html>
